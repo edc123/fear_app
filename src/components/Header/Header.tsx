532fbf2558251bca9257411d950cd728db0b605a
Edward Cheng <edc.123@hotmail.com>
2021-06-21 08:49:31 +1000

    Modified styles for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import './Header.scss'
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__text">Fēar</h1>
+      <h1 className="header__headline">Fēar</h1>
+      <h2 className="header__subheadline">
+        Nobody is safe in South Australia.
+      </h2>
     </header>
   )
 }
