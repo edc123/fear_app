5c30a414924996b0da28db4ae04c54ade6e8d9ce
Edward Cheng <edc.123@hotmail.com>
2021-06-21 06:34:46 +1000

    Added and ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import './App.scss';
+import './App.scss'
 
 import { Accordion } from './components'
 
 const App = () => {
   return (
     <div className="app">
-      <header className="header">
-        Fēar
-      </header>
+      <header className="header">Fēar</header>
       <Accordion />
     </div>
-  );
+  )
 }
 
-export { App };
+export { App }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './index.scss';
-import { App } from './App';
+// Fetch polyfill
+import 'whatwg-fetch'
+
+import './index.scss'
+import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
