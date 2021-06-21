72a009533ebab006d4e0c4d532775d3a7a5cb82f
Edward Cheng <edc.123@hotmail.com>
2021-06-22 07:55:37 +1000

    Added tests for useOffences

---
+++ src/utils/useOffences/useOffences.mock.ts
@@ -0,0 +1,99 @@
+export const mock = [
+  {
+    _id: 1,
+    'Reported Date': '01/07/2019',
+    'Suburb - Incident': 'ADELAIDE',
+    'Postcode - Incident': '5000',
+    'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+    'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+    'Offence Level 3 Description': 'Obtain benefit by deception',
+    'Offence count': '1',
+  },
+  {
+    _id: 2,
+    'Reported Date': '01/07/2019',
+    'Suburb - Incident': 'ADELAIDE',
+    'Postcode - Incident': '5000',
+    'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+    'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+    'Offence Level 3 Description':
+      'Other fraud, deception and related offences',
+    'Offence count': '1',
+  },
+  {
+    _id: 11,
+    'Reported Date': '01/07/2019',
+    'Suburb - Incident': 'ALBERTON',
+    'Postcode - Incident': '5014',
+    'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+    'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+    'Offence Level 3 Description': 'Theft from motor vehicle',
+    'Offence count': '1',
+  },
+]
+
+export const mockResult = {
+  ADELAIDE: [
+    {
+      _id: 1,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE',
+      'Postcode - Incident': '5000',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Obtain benefit by deception',
+      'Offence count': '1',
+    },
+    {
+      _id: 2,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE',
+      'Postcode - Incident': '5000',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+      'Offence Level 3 Description':
+        'Other fraud, deception and related offences',
+      'Offence count': '1',
+    },
+  ],
+  ALBERTON: [
+    {
+      _id: 11,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ALBERTON',
+      'Postcode - Incident': '5014',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Theft from motor vehicle',
+      'Offence count': '1',
+    },
+  ],
+}
+
+export const mock2 = [
+  {
+    _id: 73,
+    'Reported Date': '01/07/2019',
+    'Suburb - Incident': undefined,
+    'Postcode - Incident': '5121',
+    'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+    'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+    'Offence Level 3 Description': 'Theft from shop',
+    'Offence count': '1',
+  },
+]
+
+export const mockResult2 = {
+  'Not defined': [
+    {
+      _id: 73,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': undefined,
+      'Postcode - Incident': '5121',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Theft from shop',
+      'Offence count': '1',
+    },
+  ],
+}
