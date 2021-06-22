32c264ba3a183426d9bd095b7706b3b2aa72b63a
Edward Cheng <edc.123@hotmail.com>
2021-06-22 13:41:36 +1000

    Added mock accordion data

---
+++ src/components/Accordion/mock.ts
@@ -0,0 +1,59 @@
+export const mockData = {
+  ADELAIDE: [
+    {
+      _id: 1,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE',
+      'Postcode - Incident': '5000',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Obtain benefit by deception',
+      'Offence count': '1',
+    },
+    {
+      _id: 2,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE',
+      'Postcode - Incident': '5000',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'FRAUD DECEPTION AND RELATED OFFENCES',
+      'Offence Level 3 Description':
+        'Other fraud, deception and related offences',
+      'Offence count': '1',
+    },
+    {
+      _id: 3,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE',
+      'Postcode - Incident': '5000',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'PROPERTY DAMAGE AND ENVIRONMENTAL',
+      'Offence Level 3 Description': 'Graffiti',
+      'Offence count': '1',
+    },
+  ],
+  'ADELAIDE AIRPORT': [
+    {
+      _id: 10,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ADELAIDE AIRPORT',
+      'Postcode - Incident': '5950',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Other theft',
+      'Offence count': '1',
+    },
+  ],
+  ALBERTON: [
+    {
+      _id: 11,
+      'Reported Date': '01/07/2019',
+      'Suburb - Incident': 'ALBERTON',
+      'Postcode - Incident': '5014',
+      'Offence Level 1 Description': 'OFFENCES AGAINST PROPERTY',
+      'Offence Level 2 Description': 'THEFT AND RELATED OFFENCES',
+      'Offence Level 3 Description': 'Theft from motor vehicle',
+      'Offence count': '1',
+    },
+  ],
+}
