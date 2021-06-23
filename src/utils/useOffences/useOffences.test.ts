ca6709990084b39d25fb24b302acb2311a0e67f3
Edward Cheng <edc.123@hotmail.com>
2021-06-23 12:49:08 +1000

    Update tests for useOffences

--- src/utils/useOffences/useOffences.test.ts
+++ src/utils/useOffences/useOffences.test.ts
@@ -5,11 +5,11 @@ import { mock, mock2, mockResult, mockResult2 } from './useOffences.mock'
 describe('groupItems', () => {
   it('should return items grouped by the unique values of a specified key', () => {
     const result = groupItems(mock, 'Suburb - Incident')
-    expect(result).toBe(mockResult)
+    expect(result).toMatchObject(mockResult)
   })
 
   it('should return items under "Not Defined" if these items did not have a defined value on the specified key', () => {
     const result = groupItems(mock2, 'Suburb - Incident')
-    expect(result).toBe(mockResult2)
+    expect(result).toMatchObject(mockResult2)
   })
 })
